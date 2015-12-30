762e2e6825819a33fa2091c933bdda02bfc349ef
qianquan003 <157309341@qq.com>
2015-12-30 14:46:33 +0800

    add webserviceDemo

---
+++ study/src/test/java/com/xi/api/APIDemo.java
@@ -0,0 +1,56 @@
+package com.xi.api;
+
+import com.xi.util.PropertiesUtil;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Administrator on 2015/12/30.
+ */
+public class APIDemo {
+    public static void main(String[] args) {
+        String httpUrl = "http://apis.baidu.com/baidu_mobile_security/phone_number_service/phone_information_query";
+        String httpArg = "tel=075561550985%2C03936038331&location=true";
+        String jsonResult = request(httpUrl, httpArg);
+        System.out.println(jsonResult);
+    }
+
+    /**
+     * @param urlAll  :请求接口
+     * @param httpArg :参数
+     * @return 返回结果
+     */
+    public static String request(String httpUrl, String httpArg) {
+        BufferedReader reader = null;
+        String result = null;
+        StringBuffer sbf = new StringBuffer();
+        httpUrl = httpUrl + "?" + httpArg;
+
+        try {
+            URL url = new URL(httpUrl);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setRequestMethod("GET");
+            // 填入apikey到HTTP header
+            connection.setRequestProperty("apikey", PropertiesUtil.getProperty("apikey"));
+            connection.connect();
+            InputStream is = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String strRead = null;
+            while ((strRead = reader.readLine()) != null) {
+                sbf.append(strRead);
+                sbf.append("\r\n");
+            }
+            reader.close();
+            result = sbf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
---
+++ study/src/test/java/com/xi/util/PropertiesUtil.java
@@ -0,0 +1,68 @@
+package com.xi.util;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @version 1.0
+ */
+public class PropertiesUtil {
+    private static String default_properties = "../test-classes/config.properties";
+    private static Properties prop;
+
+    static {
+        prop = new Properties();
+        try {
+            InputStream is = new BufferedInputStream(new FileInputStream(getPath() + default_properties));
+            prop.load(is);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return prop.getProperty(key);
+    }
+
+    public static String getProperty(String key, String defaultValue) {
+        String value = prop.getProperty(key);
+        if (value == null)
+            return defaultValue;
+        return value;
+    }
+
+    public static boolean getBooleanProperty(String name, boolean defaultValue) {
+        String value = prop.getProperty(name);
+        if (value == null)
+            return defaultValue;
+        return (new Boolean(value)).booleanValue();
+    }
+
+    public static int getIntProperty(String name) {
+        return getIntProperty(name, 0);
+    }
+
+    public static double getDoubleProperty(String name) {
+        return getDoubleProperty(name, 0);
+    }
+
+    public static int getIntProperty(String name, int defaultValue) {
+        String value = prop.getProperty(name);
+        if (value == null)
+            return defaultValue;
+        return (new Integer(value)).intValue();
+    }
+
+    public static double getDoubleProperty(String name, double defaultValue) {
+        String value = prop.getProperty(name);
+        if (value == null)
+            return defaultValue;
+        return (new Double(value));
+    }
+
+    public static String getPath() {
+        return Thread.currentThread().getContextClassLoader().getResource("").getPath();
+    }
+}
