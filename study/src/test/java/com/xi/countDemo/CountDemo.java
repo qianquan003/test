43d46d540be89a4ce18969c1bcccb8d42a0c0c4b
qianquan003 <157309341@qq.com>
2016-01-05 09:44:28 +0800

    test count

--- study/src/test/java/com/xi/countDemo/CountDemo.java
+++ study/src/test/java/com/xi/countDemo/CountDemo.java
@@ -4,13 +4,23 @@ import java.text.DecimalFormat;
 
 /**
  * Created by Administrator on 2015/12/26.
+ * java中，当两个整数相除时，由于小数点以后的数字会被截断，运算结果将为整数，此时若希望得到运算结果为浮点数，必须将两整数其一或是两者都强制转换为浮点数。
+ 例如：
+ (float)a/b // 将整数其中一个强制转换为浮点数，再与另一个整数相除
+ a/(float)b
+ (float)a/(float)b // 将两个整数同时强制转换为浮点数后再相除
+ Java代码
+ float num= (float)2/3;
+ DecimalFormat df = new DecimalFormat("0.00");//格式化小数
+ String s = df.format(num);//返回的是String类型
  */
 public class CountDemo {
     public static void main(String[] args) {
-        Double a=11.11;
-        Double b=1.11;
-        DecimalFormat df=new DecimalFormat("#.00");
-        Double d=Double.valueOf(df.format(a/b));
-        System.out.println(d);
+        int a=24;
+        int b=76339;
+//        Double d=(Math.round((a/b)*10000)/10000.0);
+        DecimalFormat df=new DecimalFormat("####.0000");
+//        Double d=Double.valueOf(df.format((double)a/b));//格式化后输出的是3.0E-4
+        System.out.println(df.format((double)a/b));
     }
 }
