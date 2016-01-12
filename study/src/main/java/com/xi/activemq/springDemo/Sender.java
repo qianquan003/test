97d67f872de3bdd78a76f13b42748fc887975816
qianquan003 <157309341@qq.com>
2016-01-12 21:03:06 +0800

    add activemq web demo

--- study/src/main/java/com/xi/activemq/springDemo/Sender.java
+++ study/src/main/java/com/xi/activemq/springDemo/Sender.java
@@ -13,6 +13,7 @@ import javax.jms.Session;
 
 import org.springframework.context.ApplicationContext;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import org.springframework.jms.core.JmsTemplate;
@@ -24,7 +25,8 @@ import org.springframework.jms.core.MessageCreator;
  */
 public class Sender {
     public static void main(String[] args) {
-        ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:applicationContext-*.xml");
+        ApplicationContext ctx =new ClassPathXmlApplicationContext("applicationContext-*.xml");
+//        new FileSystemXmlApplicationContext("classpath:applicationContext-*.xml");
         JmsTemplate jmsTemplate = (JmsTemplate) ctx.getBean("jmsTemplate");
         jmsTemplate.send(new MessageCreator() {
             public Message createMessage(Session session) throws JMSException {
