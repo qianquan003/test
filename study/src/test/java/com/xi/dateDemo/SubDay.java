df3e3ade5980a963a61431b21f592c35848868d0
qianquan003 <157309341@qq.com>
2016-01-27 22:19:10 +0800

    test

--- study/src/test/java/com/xi/dateDemo/SubDay.java
+++ study/src/test/java/com/xi/dateDemo/SubDay.java
@@ -5,7 +5,47 @@ package com.xi.dateDemo;
  */
 public class SubDay {
     public static void main(String[] args) {
-        String day="2015-10-10";
-        System.out.println(day.substring(5,7));
+        String day="2015-02-10";
+        System.out.println(checkMonth(day));
+    }
+    public static int checkMonth(String day){
+        Integer days=0;
+        if(day.substring(5,7).equals("01")){
+            days=31;
+        }
+        if(day.substring(5,7).equals("02")){
+            days=28;
+        }
+        if(day.substring(5,7).equals("03")){
+            days=31;
+        }
+        if(day.substring(5,7).equals("04")){
+            days=30;
+        }
+        if(day.substring(5,7).equals("05")){
+            days=31;
+        }
+        if(day.substring(5,7).equals("06")){
+            days=30;
+        }
+        if(day.substring(5,7).equals("07")){
+            days=31;
+        }
+        if(day.substring(5,7).equals("08")){
+            days=31;
+        }
+        if(day.substring(5,7).equals("09")){
+            days=30;
+        }
+        if(day.substring(5,7).equals("10")){
+            days=31;
+        }
+        if(day.substring(5,7).equals("11")){
+            days=30;
+        }
+        if(day.substring(5,7).equals("12")){
+            days=31;
+        }
+        return days;
     }
 }
