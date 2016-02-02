050220c2eb7550edb1366a18f908c001fe13dc17
qianquan003 <157309341@qq.com>
2016-02-02 17:53:14 +0800

    add jsonDemo

--- study/src/test/java/com/xi/json/telDemo.java
+++ study/src/test/java/com/xi/json/telDemo.java
@@ -5,60 +5,47 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.apache.commons.lang.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/2/2.
  */
 public class telDemo {
     public static void main(String[] args) {
+        telDemo t = new telDemo();
+        List s = t.tel();
+        System.out.println(s);
+    }
+
+    public List tel() {
+        List list=new ArrayList();
+        String name = null;
+        String tel = null;
         String json = new ReadFileUtil().ReadFile("D:\\xm\\study\\git\\test\\study\\src\\test\\java\\com\\xi\\json\\tel.json");
         if (StringUtils.isNotEmpty(json)) {
             JSONObject dataJson = JSONObject.fromObject(json);
-//             System.out.println(dataJson);
             if (dataJson != null && dataJson.containsKey("Contacts")) {
                 JSONObject root = dataJson.getJSONObject("Contacts");
-//                 System.out.println("root:"+root);
                 if (root != null && root.containsKey("Contact")) {
                     JSONArray location = root.getJSONArray("Contact");
-//                     System.out.println("Contact:"+location);
-                    System.out.println("主人，您共有" + location.size() + "位手机联系人");
+//                    System.out.println("主人，您共有" + location.size() + "位手机联系人");
                     for (int i = 0; i < location.size(); i++) {
-//                        System.out.println("姓名：" + location.get(0));
                         if (location != null) {
                             JSONObject linkman = location.getJSONObject(i);
-                            System.out.println(linkman);
-//                            for(int j=0;j<linkman.size();j++){
-//                                String name= linkman.getString("Name");
-//                                System.out.println("姓名：" +name);
-//                            }
                             if (linkman != null && linkman.containsKey("Name")) {
-                                String name = linkman.getString("Name");
-//                                System.out.println("姓名：" +name);
-                                if (linkman != null && linkman.containsKey("PhoneList")) {
-                                    JSONObject PhoneList = linkman.getJSONObject("PhoneList");
-                                    System.out.println("PhoneList：" + PhoneList);
-                                    if (PhoneList != null && PhoneList.containsKey("Phone")) {
-                                        //只有一个电话号码时是对象
-                                        JSONObject Phone = PhoneList.getJSONObject("Phone");
-                                        System.out.println("Phone：" + Phone);
-                                        if (Phone != null) {
-                                            String tel = Phone.getString("tel");
-                                            System.out.println("姓名：" +name+" tel：" + tel);
-                                        }
-                                        //有多个电话号码时是数组
-//                                        JSONObject Phone2 = PhoneList.getJSONObject("Phone2");
-//                                        System.out.println("Phone2：" + Phone2);
-//                                        if (Phone2 != null) {
-//                                            String tel = Phone2.getString("tel");
-//                                            System.out.println("tel：" + tel);
-//                                        }
-                                        /*JSONArray Phone2 = PhoneList.getJSONArray("Phone2");
-                                        System.out.println("Phone2：" + Phone2);
-                                        if (Phone2.size()>0) {
-                                            for(int index=0;i<Phone2.size();index++){
-                                                JSONObject tel2 = Phone2.getJSONObject(index);
-                                                System.out.println("多个tel2：" + tel2);
-                                            }
-                                        }*/
+                                name = linkman.getString("Name");
+                            }
+                            if (linkman != null && linkman.containsKey("PhoneList")) {
+                                JSONObject PhoneList = linkman.getJSONObject("PhoneList");
+                                if (PhoneList != null && PhoneList.containsKey("Phone")) {
+                                    //只有一个电话号码时是对象
+                                    JSONObject Phone = PhoneList.getJSONObject("Phone");
+                                    if (Phone != null) {
+                                        tel = Phone.getString("tel");
+//                                        System.out.println("姓名：" + name + " tel：" + tel);
+                                        String info = "姓名：" + name + " tel：" + tel;
+                                        list.add(info);
                                     }
                                 }
                             }
@@ -67,5 +54,6 @@ public class telDemo {
                 }
             }
         }
+        return list;
     }
 }
