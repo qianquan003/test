e6c4245832048f511557c6f2b17829b9261a076d
qianquan003 <157309341@qq.com>
2016-03-08 17:38:40 +0800

    add demo

--- study/src/test/java/com/xi/studyDemo/B.java
+++ study/src/test/java/com/xi/studyDemo/B.java
@@ -1,10 +1,11 @@
 package com.xi.studyDemo;
 
 public class B {
-    public static void main(String[] args) {
+     static public void main(String[] args) {
         int i = 12;
 //        i += i -= i *= i;
-        System.out.println(i += i -= i *= i);
+        System.out.println(i += i -= i *= i);//等效于12+12-12*12
         System.out.println(17<<1);
     }
+    protected int method1(int a, int b) { return 0; }
 }
